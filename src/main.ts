086db95cc95be82786a2c986cc62e2ed4a11b891
Russell Blickhan <rwblickhan@gmail.com>
2021-05-07 21:36:21 -0700

    Remove spurious comments

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,6 @@ function createWindow() {
   mainWindow.webContents.openDevTools();
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", () => {
   const database = new Database()
   createWindow();
@@ -37,6 +34,3 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
